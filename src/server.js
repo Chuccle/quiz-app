ec97a5a456e800432690fd9c0704e1792cc04401
CHARLIE COHEN <CHARLIECOHEN@exe-coll.ac.uk>
2021-09-28 15:29:19 +0100

    Tidy up, more consistent indentation

--- src/server.js
+++ src/server.js
@@ -26,11 +26,17 @@ app.use('/auth', (req, res) => {
   const token = req.body.token
 
   try {
+    
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log(decoded.data)
+    
+    
     res.send({ message: decoded })
 
   } catch (err) {
+    
+    
     res.send({ error: err })
     console.log(err)
 
@@ -50,34 +56,31 @@ app.use('/retrieveStats', (req, res) => {
   const token = req.body.token
 
   try {
+    
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log(decoded.data)
-    
+
+    //We use primary key as the parameter because it guarantees a unique record without any conflicts
 
     connection.query('SELECT * FROM accounts WHERE id = ?', [decoded.data], function (error, results, fields) {
+      
+      
       if (error) throw res.send({
         error: error
-  
+
       });
 
-if (results.length > 0 && results.length < 2) {
-  console.log("works!")
-res.send({results: results })
+      // Just in case we have a conflict (two identical records existing) we perform a hard check
+      if (results.length > 0 && results.length < 2) {
+        console.log("works!")
+        res.send({ results: results })
 
 
+      }
 
 
-}
-    
-    
-    
-    
-    
     })
-
-
-
-
 
 
   } catch (err) {
@@ -100,41 +103,37 @@ app.use('/login', (req, res) => {
   // ? characters in query represent escaped placeholders for our username and password 
   connection.query('SELECT * FROM accounts WHERE username = ?', [username], function (error, results, fields) {
 
-    if (error) throw res.send({
-      Error: error
-
-    });
+    if (error) throw res.send({ Error: error });
 
     // Getting the 'response' from the database and sending it to our route. This is were the data is.
     if (results.length > 0) {
 
-      const match = bcrypt.compare(password, results[0].password)
+      const passMatch = bcrypt.compare(password, results[0].password)
+
 
       console.log(results[0].id)
 
 
-
-      if (match) {
+      if (passMatch) {
 
         //signing our token to send to client
+        //we use primary key of our record as it guaranatees a unique identifier of the record
 
-
-        const jwtToken = jwt.sign({
-          //we use primary key of our record as it guaranatees a unique identifier of the record
-          data: results[0].id
-        }, process.env.JWT_SECRET, { expiresIn: process.env.ACCESS_TOKEN_LIFE });
+        const jwtToken = jwt.sign({ data: results[0].id }, process.env.JWT_SECRET, { expiresIn: process.env.ACCESS_TOKEN_LIFE });
 
         //sending our token response back to the client
 
-        res.send({
-          token: jwtToken
-        });
+        res.send({ token: jwtToken });
+
 
       }
 
+
     }
 
+
   })
+
 
 })
 
@@ -148,9 +147,7 @@ app.use('/register', (req, res) => {
   // ? characters in query represent escaped placeholders for our username and password 
 
   connection.query('SELECT * FROM accounts WHERE username = ?', [username], function (error, results, fields) {
-    if (error) throw res.send({
-      Error: error
-    });
+    if (error) throw res.send({ Error: error });
 
 
     if (results.length === 0) {
@@ -161,34 +158,35 @@ app.use('/register', (req, res) => {
 
         connection.query('INSERT INTO accounts (username, password) VALUES (?, ?);', [username, hash], function (error, results, fields) {
           // Getting the 'response' from the database and sending it to our route. This is were the data is.
-          if (error) throw res.send({
-            Error: error
-          });
+          if (error) throw res.send({ Error: error });
+
 
         })
 
-      })
-      //signing our token to send to client
-      const jwtToken = jwt.sign({
-        data: results[0].id
 
-      }, process.env.JWT_SECRET, { expiresIn: process.env.ACCESS_TOKEN_LIFE });
+      })
+
+
+      //signing our token to send to client
+
+      const jwtToken = jwt.sign({ data: results[0].id }, process.env.JWT_SECRET, { expiresIn: process.env.ACCESS_TOKEN_LIFE });
 
       //sending our token response back to the client
 
-      res.send({
-        token: jwtToken
-      });
-
+      res.send({ token: jwtToken });
 
 
     } else {
 
+
       res.send({ error: "username already exists" })
+
 
     }
 
+
   })
+
 
 })
 
