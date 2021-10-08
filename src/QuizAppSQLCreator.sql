494182ec2bec09b15050fa24a4ba6eee4f5d5890
Chuccle <charlie11cohen@gmail.com>
2021-10-09 00:09:58 +0100

    added SQL structure to make db

---
+++ src/QuizAppSQLCreator.sql
@@ -0,0 +1,43 @@
+CREATE TABLE `accounts` (
+`id` int PRIMARY KEY AUTO_INCREMENT,
+`username` varchar(255),
+`password` varchar(255)
+);
+
+CREATE TABLE `Quizzes` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `quizname` text,
+  `created_by_userid` int
+);
+
+CREATE TABLE `Questions` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `QuizID` int,
+  `Question` text
+);
+
+CREATE TABLE `Question_Options` (
+  `id` int PRIMARY KEY,
+  `questionID` int,
+  `questionText` text,
+  `isCorrect` boolean
+);
+
+CREATE TABLE `quiz_user_answers` (
+  `id` int PRIMARY KEY,
+  `userid` int, 
+  `questionID` int,
+  `Question_Options` int
+);
+
+ALTER TABLE `Quizzes` ADD FOREIGN KEY (`created_by_userid`) REFERENCES `accounts` (`id`);
+
+ALTER TABLE `Questions` ADD FOREIGN KEY (`QuizID`) REFERENCES `Quizzes` (`id`);
+
+ALTER TABLE `Question_Options` ADD FOREIGN KEY (`questionID`) REFERENCES `Questions` (`id`);
+
+ALTER TABLE `quiz_user_answers` ADD FOREIGN KEY (`userid`) REFERENCES `accounts` (`id`);
+
+ALTER TABLE `quiz_user_answers` ADD FOREIGN KEY (`questionID`) REFERENCES `Questions` (`id`);
+
+ALTER TABLE `quiz_user_answers` ADD FOREIGN KEY (`Question_Options`) REFERENCES `Question_Options` (`id`);
