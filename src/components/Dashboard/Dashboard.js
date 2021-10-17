7dfa5d4509c0e4a504fda1073958a66da5a389c6
Chuccle <charlie11cohen@gmail.com>
2021-10-17 21:54:07 +0100

    Bug when trying to access parts of 2d arraylist

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -7,6 +7,8 @@ import Container from 'react-bootstrap/Container';
 import { TiStarOutline } from "react-icons/ti";
 import { TiStarFullOutline } from "react-icons/ti";
 import { Link } from 'react-router-dom'
+import { Array } from 'core-js';
+import { useEffect } from 'react';
 
 
 
@@ -40,56 +42,85 @@ async function getUserData(credentials) {
 
 export default function Dashboard() {
   // I could neaten this up to one usestate hook call but this is more readable
-  // const [email, SetEmail] = useState()
-  const [id, SetId] = useState()
-  const [username, setUserName] = useState()
 
-
+  const [data, SetData] = useState()
 
   const { token } = useToken();
 
-  async function SetStatsfunc() {
+  
+  
+  
+  useEffect(() => {
+    if (!data) {
+
+      async function SetStatsfunc() {
+        const StatsArray = []
+
+        try {
+          const userStats = await getUserData({ token })
 
 
-    //  console.log(token)
-    const userStats = await getUserData({ token })
+          if (userStats.error) {
 
-
-    try {
-
-      if (userStats.error) {
-
-        alert("The server was unable to verify your session")
+            alert("The server was unable verify your identity")
 
 
 
+          }
+          else if (userStats.results) {
+
+            //We destructure our array of objects into an 2d arraylist of values
+
+            const objectArray = (userStats.results)
+
+            objectArray.forEach(value => {
+
+              StatsArray.push(Object.values(value))
+
+            });
+
+            //on our 2d arraylist we cleanup the data on score
+            StatsArray.forEach(value => {
+
+              if (value[2] == null) {
+
+                value[2] = 0
+              }
+
+              
+
+              SetData(StatsArray)
+
+            
+
+
+
+            });
+
+
+
+
+
+          }
+
+        } catch {
+
+          alert("A server error occurred")
+
+
+        }
       }
-      else if (userStats.results) {
-
-        //change this to quiz score stats and display it on dashboard
-        //  console.log(userStats.results)
-        // SetEmail(userStats.results[0].email)
-        SetId(userStats.results[0].id)
-        setUserName(userStats.results[0].username)
 
 
-
-      }
-
-    } catch {
-
-      alert("A server error occurred")
-
+      SetStatsfunc()
 
     }
+  })
 
-  }
+  
 
-  SetStatsfunc()
-
-
-  //console.log(username)
-  // console.log(email)
+  // console.log(quizname)
+  // console.log(score)
   // console.log(id)
 
 
@@ -98,7 +129,7 @@ export default function Dashboard() {
 
       <Jumbotron fluid>
 
-        <h1 className="header">Welcome to your dashboard: {username}</h1>
+        <h1 className="header">Welcome to your dashboard: {data}</h1>
         <h5>Please select a quiz</h5>
 
       </Jumbotron>
@@ -114,23 +145,23 @@ export default function Dashboard() {
         </thead>
         <tbody>
           <tr>
-            <td>C++</td>
+            <td>{data}</td>
             <td class="col_spacer" > Easy <Container><TiStarFullOutline /> < TiStarOutline /> < TiStarOutline /></Container> </td>
-            <td>{id}%</td>
+            <td>{data}%</td>
             <td><Link to='/quizzes/c++'>Start</Link>   </td>
 
           </tr>
           <tr>
-            <td>Java</td>
+            <td>{data}</td>
             <td class="col_spacer">Hard <Container><TiStarFullOutline /> <TiStarFullOutline />   <TiStarFullOutline /> </Container> </td>
-            <td>{id}%</td>
+            <td>{data}%</td>
             <td><Link to='/quizzes/c++'>Start</Link>   </td>
 
           </tr>
           <tr>
-            <td>PHP</td>
+            <td>{data}</td>
             <td class="col_spacer">Intermediate <Container> <TiStarFullOutline /> <TiStarFullOutline /> < TiStarOutline /> </Container> </td>
-            <td>{id}%</td>
+            <td>{data}%</td>
             <td><Link to='/quizzes/c++'>Start</Link>   </td>
 
           </tr>
