1c6a273ed14e0f6571e9ed91a95c1acba7de72e1
Chuccle <charlie11cohen@gmail.com>
2021-10-25 23:59:49 +0100

    Quiz now is working, just needs score insert

--- src/components/Quizzes/C++/Quiz.js
+++ src/components/Quizzes/C++/Quiz.js
@@ -29,6 +29,7 @@ export default function Quiz() {
 
 const {token} = useToken()
 	const quizdata = useLocation()
+	console.log(quizdata.state.quizid)
 	const quizid = quizdata.state.quizid
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showScore, setShowScore] = useState(false);
@@ -38,39 +39,49 @@ const {token} = useToken()
 	
 	
 	
-async function getquestiondata () {
-
+	
+	
+	
+	
+	useEffect(() => {
 		if (!questiondata) {
-		const question = await fetchQuestions({token, quizid}) 
+	async function getquestiondata () {
 
-   
+
+   const question = await fetchQuestions({token, quizid}) 
+
+ 
 
  setQuestionData(question)
 
 
- console.log (question)
 }
-	}
+
+getquestiondata()
+		}
+		})
+
+		
+	
 
 
-
-	getquestiondata()
 		
 
 
-
-const questions = [
+if (questiondata) {
+	console.log (questiondata.questions.length)
+		
+const questions = 
 	{
-		questionText: 'What is type of programming language is C++?',
+		questionText: questiondata.questions[currentQuestion].Questiontext,
 		answerOptions: [
-			{ answerText: 'Procedural-oriented', isCorrect: false },
-			{ answerText: 'Functional', isCorrect: false },
-			{ answerText: 'Object-oriented', isCorrect: true },
-			{ answerText: 'Scripting', isCorrect: false },
+			{ answerText: questiondata.questions[currentQuestion].Options.Correct, isCorrect: true},
+			{ answerText: questiondata.questions[currentQuestion].Options.Incorrect1, isCorrect: false },
+			{ answerText: questiondata.questions[currentQuestion].Options.Incorrect2, isCorrect: false },
+			{ answerText: questiondata.questions[currentQuestion].Options.Incorrect3, isCorrect: false },
 		],
-	},
+	}
 	
-];
 
 
 	const handleAnswerOptionClick = (isCorrect) => {
@@ -79,29 +90,32 @@ const questions = [
 		}
 
 		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
+		if (nextQuestion < questiondata.questions.length) {
 			setCurrentQuestion(nextQuestion);
 
 		} else {
 			setShowScore(true);
 		}
 	};
+
+
+
 	return (
 		<div className='app'>
 			{showScore ? (
 				<div className='score-section'>
-					You scored {score} out of {questions.length}
+					You scored {score} out of {questiondata.questions.length}
 				</div>
 			) : (
 				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
+				<div className='question-section'>					
+					<div className='question-count'>
+							<span>Question{currentQuestion + 1}</span>/{questiondata.questions.length}
 						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+						<div className='question-text'>{questions.questionText}</div>
 					</div>
 					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
+						{questions.answerOptions.map((answerOption) => (
 							<button className='quizbutton' onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
 						))}
 					</div>
@@ -109,6 +123,15 @@ const questions = [
 			)}
 		</div>
 	);
-}
+} else {
+	return (
+		<div>Loading..</div>
+			)
 
+
+
+
+
+}
+}
 
