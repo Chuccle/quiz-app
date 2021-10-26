f13f47c68f9c1d4c24b98375e2aedfc955f912f3
Chuccle <charlie11cohen@gmail.com>
2021-10-26 22:01:03 +0100

    linting

--- src/components/Quizzes/C++/Quiz.js
+++ src/components/Quizzes/C++/Quiz.js
@@ -7,34 +7,34 @@ import useToken from '../../App/useToken';
 async function fetchQuestions(data) {
 
 	return fetch('http://localhost:8080/retrievequestions', {
-	  method: 'POST',
-	  headers: {
-		'Content-Type': 'application/json'
-  
-	  },
-  
-	  body: JSON.stringify(data)
-  
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+
+		},
+
+		body: JSON.stringify(data)
+
 	})
-  
-	  .then(data => data.json())
-  
-  }
-  
+
+		.then(data => data.json())
+
+}
+
 async function sendResults(data) {
 
 	return fetch('http://localhost:8080/sendresults', {
-	  method: 'POST',
-	  headers: {
-		'Content-Type': 'application/json'
-  
-	  },
-  
-	  body: JSON.stringify(data)
-  
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+
+		},
+
+		body: JSON.stringify(data)
+
 	})
-  
-	  .then(data => data.json())
+
+		.then(data => data.json())
 
 
 
@@ -45,7 +45,7 @@ async function sendResults(data) {
 export default function Quiz() {
 
 
-const {token} = useToken()
+	const { token } = useToken()
 	const quizdata = useLocation()
 	console.log(quizdata.state.quizid)
 	const quizid = quizdata.state.quizid
@@ -53,124 +53,124 @@ const {token} = useToken()
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
 	const [questiondata, setQuestionData] = useState()
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
 	useEffect(() => {
 		if (!questiondata) {
-	async function getquestiondata () {
+			async function getquestiondata() {
 
 
-   const question = await fetchQuestions({token, quizid}) 
-
- 
-
- setQuestionData(question)
+				const question = await fetchQuestions({ token, quizid })
 
 
-}
 
-getquestiondata()
+				setQuestionData(question)
+
+
+			}
+
+			getquestiondata()
 		}
-		})
-
-		
-	
+	})
 
 
-		
 
 
-if (questiondata) {
-	console.log (questiondata.questions.length)
-		
-const questions = 
-	{
-		questionText: questiondata.questions[currentQuestion].Questiontext,
-		answerOptions: [
-			{ answerText: questiondata.questions[currentQuestion].Options.Correct, isCorrect: true},
-			{ answerText: questiondata.questions[currentQuestion].Options.Incorrect1, isCorrect: false },
-			{ answerText: questiondata.questions[currentQuestion].Options.Incorrect2, isCorrect: false },
-			{ answerText: questiondata.questions[currentQuestion].Options.Incorrect3, isCorrect: false },
-		],
-	}
-	
-	
 
 
-	const handleAnswerOptionClick = (isCorrect) => {
-		if (isCorrect) {
-			setScore(score + 1);
+
+
+	if (questiondata) {
+		console.log(questiondata.questions.length)
+
+		const questions =
+		{
+			questionText: questiondata.questions[currentQuestion].Questiontext,
+			answerOptions: [
+				{ answerText: questiondata.questions[currentQuestion].Options.Correct, isCorrect: true },
+				{ answerText: questiondata.questions[currentQuestion].Options.Incorrect1, isCorrect: false },
+				{ answerText: questiondata.questions[currentQuestion].Options.Incorrect2, isCorrect: false },
+				{ answerText: questiondata.questions[currentQuestion].Options.Incorrect3, isCorrect: false },
+			],
 		}
 
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questiondata.questions.length) {
-			setCurrentQuestion(nextQuestion);
 
-		} else {
-		
-			
-			setShowScore(true);
-			
+
+
+		const handleAnswerOptionClick = (isCorrect) => {
+			if (isCorrect) {
+				setScore(score + 1);
+			}
+
+			const nextQuestion = currentQuestion + 1;
+			if (nextQuestion < questiondata.questions.length) {
+				setCurrentQuestion(nextQuestion);
+
+			} else {
+
+
+				setShowScore(true);
+
+
+			}
+
+		};
+
+		let results
+
+		// assign results after quiz is finished 
+		if (showScore) {
+
+			results = (score / questiondata.questions.length) * 100
+			sendResults({ token, results, quizid })
 
 		}
 
-	};
 
-	let results
-	
-	// assign results after quiz is finished 
-	if (showScore) {
-
-		results = (score/questiondata.questions.length)*100
-		sendResults({token, results, quizid })
-	
-}
+		return (
+			<div className='app'>
+				{showScore ?
 
 
-return (
-		<div className='app'>
-			{showScore ?
-			 
-			 
-			 
-			 (
-				
-				<div className='score-section'>
-					You scored {results}% ({(score)}/{questiondata.questions.length})
-				</div>
-			) : (
-				<>
-				<div className='question-section'>					
-					<div className='question-count'>
-							<span>Question{currentQuestion + 1}</span>/{questiondata.questions.length}
+
+					(
+
+						<div className='score-section'>
+							You scored {results}% ({(score)}/{questiondata.questions.length})
 						</div>
-						<div className='question-text'>{questions.questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions.answerOptions.map((answerOption) => (
-							<button className='quizbutton' onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))}
-					</div>
-				</>
-			)}
-		</div>
-	);
-} else {
-	return (
-		<div>Loading..</div>
-			)
+					) : (
+						<>
+							<div className='question-section'>
+								<div className='question-count'>
+									<span>Question{currentQuestion + 1}</span>/{questiondata.questions.length}
+								</div>
+								<div className='question-text'>{questions.questionText}</div>
+							</div>
+							<div className='answer-section'>
+								{questions.answerOptions.map((answerOption) => (
+									<button className='quizbutton' onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+								))}
+							</div>
+						</>
+					)}
+			</div>
+		);
+	} else {
+		return (
+			<div>Loading..</div>
+		)
 
 
 
 
 
-}
+	}
 }
 
--- src/server.js
+++ src/server.js
@@ -280,7 +280,7 @@ app.use('/insertquiz', (req, res) => {
       })
 
     })
-    
+
     res.send({
       QuizStatus: "Inserted"
     })
@@ -374,35 +374,38 @@ app.use('/sendresults', (req, res) => {
 
 
   const decodedtoken = jwt.verify(req.body.token, process.env.JWT_SECRET);
-  
-connection.query('Select * from quiz_user_answers where userid = ? And quizid = ?' , [decodedtoken.data, req.body.quizid], function (error, results, fields) {
+
+  connection.query('Select * from quiz_user_answers where userid = ? And quizid = ?', [decodedtoken.data, req.body.quizid], function (error, results, fields) {
     if (error) throw res.send({
       Error: error
     });
 
-if (results.length===0) {
-  connection.query('insert into quiz_user_answers(userid, quizid, score) values (?,?,?)' , [decodedtoken.data, req.body.quizid, req.body.results], function (error, results, fields) {
-    if (error) throw res.send({
-      Error: error
-    });
+    if (results.length === 0) {
+      connection.query('insert into quiz_user_answers(userid, quizid, score) values (?,?,?)', [decodedtoken.data, req.body.quizid, req.body.results], function (error, results, fields) {
+        if (error) throw res.send({
+          Error: error
+        });
+      })
+
+    } else {
+
+
+      if (results[0].score < req.body.results) {
+        //by using update we can reduce the amount of records overall, the alternative is multiple records with different scores
+        connection.query('UPDATE quiz_user_answers SET score = ? WHERE id = ?', [req.body.results, results[0].id], function (error, results, fields) {
+          if (error) throw res.send({
+            Error: error
+          });
+        })
+
+      }
+    }
+
   })
 
-} else {
-
-if (results[0].score<req.body.results) {
-
-  connection.query('UPDATE quiz_user_answers SET score = ? WHERE id = ?' , [req.body.results, results[0].id], function (error, results, fields) {
-    if (error) throw res.send({
-      Error: error
-    });
+  res.send({
+    status: "ok"
   })
-
-}
-}
-
-})
-
-res.send({status: "ok"})
 
 })
 
